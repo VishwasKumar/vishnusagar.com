3f1633ba98be9f8801f417b4f7b829fa1f86c5ea
Vishwas <vishwaskumar58@gmail.com>
2015-12-14 18:19:54 -0800

    added psql support

--- pages/admin/include/db_con.php
+++ pages/admin/include/db_con.php
@@ -1,6 +1,6 @@
 <?php
-$con=mysql_connect("localhost","root","");
-mysql_select_db("assignment",$con) or die(mysql_error($con));
+$con = pg_connect("host=ec2-54-83-59-110.compute-1.amazonaws.com port=5432 dbname=d8h402rrjiluoa user=gxmemxqrkaujqo password=ph3wu2QeU344cQsRFFi5QGtqF-");
+// mysql_select_db("assignment",$con) or die(mysql_error($con));
 //mysql_select_db("assignment",$con) or die("Couldn't connect to the database"); //MS
 // error_reporting(E_ALL ^ E_NOTICE);
 ?>
